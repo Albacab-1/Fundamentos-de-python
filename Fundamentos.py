7e14015fc79b3e19a801b372d43cca0372dbb9d8
Albacab-1 <fabscabrera@gmail.com>
2025-05-31 13:10:18 -0600

    Create Fundamentos.py
    Se agregan comparadores lógicos, booleanos y aritméticos

---
+++ Fundamentos.py
@@ -0,0 +1,37 @@
+if __name__ == "__main__":
+
+    print ("------ operadores matemáticos-----")
+    print ("operador suma, 5 + 6 =", 5 + 6)
+    print ("operador resta, 9 - 8 =", 9 - 8)
+    print ("operador multiplicación, 5 * 8 =", 5 * 8)
+    print ("operador división entera, 13 // 7 =", 13 // 7)
+    print ("operador división, 3/2 =",  3/2)
+    print ("operador módulo, 21%7 =",  21%7)
+    print ("operador cambio de signo, -2 =",  -2)
+    print ("operador potencia, 2**3 =",  2**3) # comentarios línea por línea
+    ''' comentarios
+    multi
+    líneas'''
+
+    print ("hola fabiola")
+
+
+    print ("------operadores lógicos-----")
+    print ("True and True:",True and True)
+    print ("False and False:",False and False)
+    print ("True and False:",True and False)
+    print ("True or False:", True or False)
+    print ("True or True:", True or True)
+    print ("False or false:", False or False)
+    print ("not False:", not  False)
+    print ("not True:", not  True)
+
+
+    print ("----Operadores de comparación")
+    print ("Igualdad, 4 == 3",4 == 3)
+    print ("Diferente, 4 != 3",4 != 3)
+    print ("Mayor, 4 > 3",4 > 3)
+    print ("Mayor o igual, 4 >= 5",4 >= 5)
+    print ("Menor, 2 < 3",2 < 3)
+    print ("Menor o igual, 3 >= 3", 3 <= 3)
+
